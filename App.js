cde8f0e63f1ba01087eee5fb91c550c8bc3af5c3
YoHypixel <78453373+YoHypixel@users.noreply.github.com>
2021-09-16 16:34:49 -0600

    something Changed I guess

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-export default function App() {
+const button = () => {
+  alert('button Pressed')
+}
+
+
+const App = () => {
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={button} title="Wow"/>
+
+      <Text>Caden's React Native Project</Text>
+      <TextInput 
+
+      keyboardType='email-address'
+      style={{height: 40, padding: 10}} 
+      placeholder="Type Here"
+      onChangeText={text => setText(text)}
+      defaultValue={text}
+
+      />
+
+      <Text style={{padding: 10, fontSize: 42}}> 
+      
+        {1+1}
+
+      </Text>
+
+      <StatusBar style="dark" />
     </View>
   );
 }
+export default App
 
 const styles = StyleSheet.create({
   container: {
