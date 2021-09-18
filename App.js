a529177406065b5f3908cca8958cf1090df36e44
YoHypixel <78453373+YoHypixel@users.noreply.github.com>
2021-09-18 14:24:56 -0600

    IT WORKS

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Keyboard, TouchableWithoutFeedback} from 'react-native';
+
+
 
 
 
@@ -10,27 +12,54 @@ const [number, SetNumber] = useState("");
 const [number2, SetNumber2] = useState("");
 const [test, SetTest] = useState("")
 
-const testlol = () => {
+const Alert = () => {
   alert(parseInt(number) + parseInt(number2))
+  Keyboard.dismiss()
 }
 
+const DismissKeyboard = ({ children }) => (
+
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
+
   return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     <View style={styles.container}>
+
       <StatusBar style="dark" />
      
         <Text>test</Text>
         <Button 
 
-        onPress={testlol}
+        onPress={Alert}
         title="test"
         />
         <Text>Number 1</Text>
-        <TextInput defaultValue="0" keyboardType='number-pad' onChangeText={text => SetNumber(text)}/>        
+        <TextInput 
+
+        defaultValue="0" 
+        keyboardType='number-pad' 
+        onChangeText={text => SetNumber(text)}
+        clearTextOnFocus={true}
+        onEndEditing={Keyboard.dismiss()}
+
+        />        
         
         <Text>Number 2</Text>
-        <TextInput defaultValue="0" keyboardType='number-pad' onChangeText={text => SetNumber2(text)}/>
+        <TextInput 
+        
+        defaultValue="0" 
+        keyboardType='number-pad' 
+        onChangeText={text => SetNumber2(text)}
+        clearTextOnFocus={true}
+        onEndEditing={Keyboard.dismiss()}
+
+        />
 
     </View>
+</TouchableWithoutFeedback>
   );
 }
 export default App
